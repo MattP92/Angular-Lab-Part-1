66e5e0f0d468a63633d7780cef836075ea6b9ad8
MattP92 <39284208+MattP92@users.noreply.github.com>
2018-10-08 20:32:02 -0400

    Update and rename familyMembers.js to toDo.js

--- components/main/familyMembers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-{
-    angular.module('app')
-        .component('main', {
-            controller: 'familyMembers',
-            templateUrl: 'components/main/familyMembers.html'
-        })
-}
-
